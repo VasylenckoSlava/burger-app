41963d8d94f8bd1da831e2c94828690eed36963a
Slava V <vyacheslav.vasylencko@itomy.ch>
2019-01-12 09:17:45 +0200

    Styled the app for mobile devices

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,14 @@
 import React from "react";
-import classes from "./Tollbar.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./Tollbar.css";
 
 const toolbar = (props) => {
   return (
-    <header className={classes.Toolbar}>
+    <header className={classes.Toolbar} height="80%">
       <div>MENU</div>
       <Logo />
-      <nav>
+      <nav className={classes.DesktopOnly}>
         <NavigationItems />
       </nav>
     </header>
