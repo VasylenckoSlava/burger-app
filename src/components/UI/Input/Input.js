1c33f630615f1530a11a89b35d70d74cba0ad2dc
Nikita S <nikita.subbotin@itomy.ch>
2019-02-24 12:01:33 +0200

    Created additional logic for form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,27 +1,61 @@
-import React from 'react';
-import classes from './Input.css';
+import React from "react";
+import classes from "./Input.css";
 
-const Input = (props) => {
-    let inputElement = null;
+const Input = props => {
+  let inputElement = null;
 
+  switch (props.elementType) {
+    case "input":
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case "textarea":
+      inputElement = (
+        <textarea
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case "select":
+      inputElement = (
+        <select
+          className={classes.InputElement}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.displayValue}
+            </option>
+          ))}
+        </select>
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
+  }
 
-    switch (props.inputtype) {
-        case ('input'):
-            inputElement = <input className={classes.InputElement} {...props} />;
-            break;
-        case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props} />;
-            break;
-        default:
-            inputElement = <input className={classes.InputElement} {...props} />
-    }
-
-    return (
-        <div className={classes.Input}>
-            <label className={classes.Label}>{props.label}</label>
-            {inputElement}
-        </div>
-    );
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+    </div>
+  );
 };
 
 export default Input;
